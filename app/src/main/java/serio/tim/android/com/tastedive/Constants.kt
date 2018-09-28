45b842169a781609c42e268c4a45b3b9e2ac5a89
TimSerio32 <timserio32@gmail.com>
2018-09-28 12:51:33 -0500

    Initial UI commit.

---
+++ app/src/main/java/serio/tim/android/com/tastedive/Constants.kt
@@ -0,0 +1,7 @@
+package serio.tim.android.com.tastedive
+
+object Constants {
+    val API_URL = "https://www.tastedive.com/api/"
+    val API_KEY = "318942-TasteDiv-NRT894SK"
+    val SPINNER_ITEMS: List<String> = listOf("Mixed", "Music", "Movies", "Shows", "Books", "Authors", "Games")
+}
--- app/src/main/java/serio/tim/android/com/tastedive/MainActivity.kt
+++ app/src/main/java/serio/tim/android/com/tastedive/MainActivity.kt
@@ -2,11 +2,33 @@ package serio.tim.android.com.tastedive
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val spinnerItems: List<String> = Constants.SPINNER_ITEMS
+
+    private var type: String = spinnerItems[0]
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progress_loadingIndicator.hide()
+
+        setUpSpinner()
+    }
+
+    fun requestOnClick(v: View) {
+
+    }
+
+    private fun setUpSpinner() {
+        spinner.setItems(spinnerItems)
+
+        spinner.setOnItemSelectedListener { _, position, _, _ ->
+            type = spinnerItems[position]
+        }
     }
 }
